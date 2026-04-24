5830994a3d6ce9c4b27ba37e90ebdc2dbe9ae5ed
F20LAB207E02 <F20LAB207E02@192.168.127.53>
2026-04-24 10:23:26 -0500

    primer commit: clase operacion

---
+++ src/main/java/operacion/operacion.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operacion;
+
+/**
+ *
+ * @author F20LAB207E02
+ */
+public class operacion {
+    private  int op1;
+    private  int op2;
+    
+    //constructores:Vvv
+    public operacion(int a, int b){
+        op1 = a;
+        op2 = b;
+    }
+    
+    public void setOp1(int op1){
+        this.op1 = op1;
+    }
+    
+    public void setOp2(int op2){
+        this.op2 = op2;
+    }
+    
+    //imprimir operadores
+    public void printOps(){
+        System.out.println("operador 1: " + op1);
+        System.out.println("operador 2: " + op2);
+    }
+    
+    //operacionesxddd
+    public  int sumar(){
+        return op1 + op2;
+    }
+    
+    
+}
