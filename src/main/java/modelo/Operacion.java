25f767d36b13aec033bfc24938e24932c202827c
manuelsantacruz <manuel202x@hotmail.com>
2026-04-27 22:55:19 -0500

    actualizacion modelo

--- src/main/java/modelo/Operacion.java
+++ src/main/java/modelo/Operacion.java
@@ -1,4 +1,4 @@
-/*
+https://github.com/manueeel202x/2026sessions/tree/master/modelo/src/main/java/modelo/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
